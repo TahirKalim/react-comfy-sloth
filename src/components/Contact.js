8efdfaec7c2222259d5200eac1cc3c5b4dec718a
tahir kalim <thrkalim@gmail.com>
2021-08-06 00:06:57 +0200

    home page- contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,27 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className='section-center'>
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className='content'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit ut
+            eligendi aut, ad laboriosam ex vel fugiat harum quaerat sunt.
+          </p>
+          <form className='contact-form'>
+            <input
+              type='email'
+              className='form-input'
+              placeholder='enter email'
+            />
+            <button className='submit-btn'>subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
