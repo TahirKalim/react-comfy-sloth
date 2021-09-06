d2350bcec26f86af010a3d53e855426585e45730
tahir kalim <thrkalim@gmail.com>
2021-09-06 03:49:35 +0200

    sort component view button

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -3,15 +3,20 @@ import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
 const Sort = () => {
-  const { filtered_products: products, grid_view } = useFilterContext()
+  const {
+    filtered_products: products,
+    grid_view,
+    setGridView,
+    setListView,
+  } = useFilterContext()
   return (
     <Wrapper>
       <div className='btn-container'>
         <button type='button' className={`${grid_view ? 'active' : null}`}>
-          <BsFillGridFill />
+          <BsFillGridFill onClick={setGridView} />
         </button>
         <button type='button' className={`${!grid_view ? 'active' : null}`}>
-          <BsList />
+          <BsList onClick={setListView} />
         </button>
       </div>
       <p>{products.length} products found</p>
